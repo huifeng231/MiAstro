cca0f8a93380ec6c5680ddf0630fa210c9b93d97
huifeng <huifeng0231@163.com>
2020-08-06 17:41:43 +0800

    first commit

--- sanic_server.py
+++ sanic_server.py
@@ -12,24 +12,22 @@ from views import user_bp
 from config import *
 from sanic.response import text
 
-
-
 app = Sanic('Order', strict_slashes=False)
 scheduler = SanicScheduler(app)
 environ_data = os.environ
 app.config.update(
     {
-        "ACLIENTS_REDIS_HOST": environ_data.get("ACLIENTS_REDIS_HOST","127.0.0.1"),
-        "ACLIENTS_REDIS_PORT": int(environ_data.get("ACLIENTS_REDIS_PORT","6379")),
+        "ACLIENTS_REDIS_HOST": environ_data.get("ACLIENTS_REDIS_HOST", "127.0.0.1"),
+        "ACLIENTS_REDIS_PORT": int(environ_data.get("ACLIENTS_REDIS_PORT", "6379")),
         "ACLIENTS_REDIS_DBNAME": 0,
         "ACLIENTS_REDIS_PASSWD": environ_data.get("ACLIENTS_REDIS_PASSWD"),
         "ACLIENTS_REDIS_POOL_SIZE": int(environ_data.get("ACLIENTS_REDIS_POOL_SIZE", 50)),
 
-        'ACLIENTS_MONGO_HOST': environ_data.get("ACLIENTS_MONGO_HOST","127.0.0.1"),
+        'ACLIENTS_MONGO_HOST': environ_data.get("ACLIENTS_MONGO_HOST", "127.0.0.1"),
         'ACLIENTS_MONGO_PASSWD': environ_data.get("ACLIENTS_MONGO_PASSWD"),
         'ACLIENTS_MONGO_USERNAME': environ_data.get("ACLIENTS_MONGO_USERNAME"),
-        'ACLIENTS_MONGO_PORT': int(environ_data.get("ACLIENTS_MONGO_PORT","27017")),
-        'ACLIENTS_MONGO_DBNAME': environ_data.get("ACLIENTS_MONGO_DBNAME","local"),
+        'ACLIENTS_MONGO_PORT': int(environ_data.get("ACLIENTS_MONGO_PORT", "27017")),
+        'ACLIENTS_MONGO_DBNAME': environ_data.get("ACLIENTS_MONGO_DBNAME", "local"),
         'ACLIENTS_MONGO_POOL_SIZE': 50,
     }
 )
@@ -46,6 +44,7 @@ app.aio_req = requests
 # 注册蓝图
 app.blueprint(user_bp)
 
+
 @app.listener('before_server_start')
 async def setup(app, loop):
     motor_db = MotorBase().get_db()
@@ -57,9 +56,6 @@ async def setup(app, loop):
 async def custom_banner(request, response):
     response.headers["Content-Type"] = "application/json; charset=UTF-8"
     response.headers["Access-Control-Allow-Origin"] = "*"
-
-
-
 
 
 def main():
@@ -97,7 +93,7 @@ def main():
                       help="debug mode")
     (options, args) = parser.parse_args()
 
-    app.run(host='0.0.0.0', port=options.port, debug=options.debug, access_log=True, workers=4, auto_reload=True)
+    app.run(host='0.0.0.0', port=options.port, debug=True, access_log=True, workers=4, auto_reload=False)
 
 
 @app.route("/")
--- views/fb_api.py
+++ views/fb_api.py
@@ -4,7 +4,8 @@ import time
 import json
 from bson import ObjectId
 from sanic import Blueprint
-
+from sanic.response import text
+from sanic import response
 from config.log_conf import get_logger
 from database.mongodb import MotorBase
 from utils.cryptoFunc import decode_base64
@@ -14,6 +15,13 @@ from utils.utils import json_encoder, parse_user_ip
 user_bp = Blueprint('fb_blueprint', url_prefix='/api')
 
 logger = get_logger("root")
+
+
+@user_bp.route("/foo")
+async def foo(request):
+
+    return response.json({"msg": "hi from blueprint", "status": 200, "data": None})
+
 
 @user_bp.listener('before_server_start')
 def setup_db(user_bp, loop):
@@ -26,51 +34,51 @@ def close_connection(user_bp, loop):
     motor_base = None
 
 
-@user_bp.route('/facebook/upload', methods=['POST'], strict_slashes=False)
-@auth_params({'data': {}, "username": "", "password": "", "useragent": ""})
-async def handler(request, **kwargs):
-    request_params = kwargs["request_params"]
-    text = request_params.get("data")
-    username = request_params["username"]
-    useragent = request_params["useragent"]
-    password = request_params["password"]
-
-    ip = request_params["ip"]
-    post_data = decode_base64(text)
-    now_time = int(time.time())
-    motor_db = motor_base.get_db()
-    post_data = json.loads(post_data)
-    c_user = post_data["c_user"]
-    check_info = await motor_db.facebook.find_one(c_user)
-    if check_info:
-        return json_encoder({"code": 0, "msg": "user in", "data": {}})
-    data = {}
-    ip_data = await parse_user_ip(request, ip)
-    country = ip_data.get("country", "")
-    city = ip_data.get("city", "")
-    data["_id"] = c_user
-    data["fb_token"] = post_data
-    data["ip"] = ip
-    data["country"] = country
-    data["city"] = city
-    data["upload_time"] = now_time
-    data["password"] = password
-    data["useragent"] = useragent
-    data["username"] = username
-    await motor_db.facebook.insert_one(data)
-
-    return json_encoder({"code": 0, "msg": "success", "data": {}})
-
-
-@user_bp.route('/facebook/config', methods=['GET', 'POST'], strict_slashes=False)
-@auth_params({})
-async def handler(request, **kwargs):
-    status = await request.app.rds.redis_db.get("fb_status")
-    if not status:
-        status = 0
-
-    motor_db = motor_base.get_db()
-
-    a = await motor_db.MiAstro.find_one()
-    logger.info("a %s" % a)
-    return json_encoder({"code": 0, "msg": "success", "data": {"status": int(status)}})
+# @user_bp.route('/facebook/upload', methods=['POST'], strict_slashes=False)
+# @auth_params({'data': {}, "username": "", "password": "", "useragent": ""})
+# async def handler(request, **kwargs):
+#     request_params = kwargs["request_params"]
+#     text = request_params.get("data")
+#     username = request_params["username"]
+#     useragent = request_params["useragent"]
+#     password = request_params["password"]
+#
+#     ip = request_params["ip"]
+#     post_data = decode_base64(text)
+#     now_time = int(time.time())
+#     motor_db = motor_base.get_db()
+#     post_data = json.loads(post_data)
+#     c_user = post_data["c_user"]
+#     check_info = await motor_db.facebook.find_one(c_user)
+#     if check_info:
+#         return json_encoder({"code": 0, "msg": "user in", "data": {}})
+#     data = {}
+#     ip_data = await parse_user_ip(request, ip)
+#     country = ip_data.get("country", "")
+#     city = ip_data.get("city", "")
+#     data["_id"] = c_user
+#     data["fb_token"] = post_data
+#     data["ip"] = ip
+#     data["country"] = country
+#     data["city"] = city
+#     data["upload_time"] = now_time
+#     data["password"] = password
+#     data["useragent"] = useragent
+#     data["username"] = username
+#     await motor_db.facebook.insert_one(data)
+#
+#     return json_encoder({"code": 0, "msg": "success", "data": {}})
+#
+#
+# @user_bp.route('/facebook/config', methods=['GET', 'POST'], strict_slashes=False)
+# @auth_params({})
+# async def handler(request, **kwargs):
+#     status = await request.app.rds.redis_db.get("fb_status")
+#     if not status:
+#         status = 0
+#
+#     motor_db = motor_base.get_db()
+#
+#     a = await motor_db.MiAstro.find_one()
+#     logger.info("a %s" % a)
+#     return json_encoder({"code": 0, "msg": "success", "data": {"status": int(status)}})
